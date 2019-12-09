515cfba8ffd7c60a7d7bfcabaeb90c23018c8052
saranshgakhar <saransh.gakhar1@gmail.com>
2019-12-09 11:22:12 +0530

    Updates

---
+++ precache-manifest.93d72c0ef093024c5289cc60ec1d0cd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1dc829c5757d0a5ffe130b2fd85cab3",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "edad4a29b3cdc47d8f6c",
+    "url": "/react-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b9386138d766d2bbede8",
+    "url": "/react-test/static/js/2.9747f88b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-test/static/js/2.9747f88b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "edad4a29b3cdc47d8f6c",
+    "url": "/react-test/static/js/main.30ffd9ea.chunk.js"
+  },
+  {
+    "revision": "cd9e5f90c41cbea25071",
+    "url": "/react-test/static/js/runtime-main.81c86ed4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
